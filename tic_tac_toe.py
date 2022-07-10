81f902e2d72f0a1f6c54ee2794b2004e10a3ec8e
japolyak <artemkryvolap@gmail.com>
2022-07-10 19:51:04 +0200

    Tic-Tac-Toe 3.0
    The main update of the game is option to choose, what to do.
    To start the game you should input three words separated by whitespaces
       * The first word should be **start**
       * The second and the third word should be **easy** or **user**, where **easy** to play as an AI, and **user** to play as a human
       * An order is important - the second word marks "X" player, and the third word marks "O" player
    If you input only **exit** it simply ends the program

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,25 @@
 from random import randrange
 
+game_field = [["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+              ["|", " ", " ", " ", " ", " ", " ", " ", "|"],
+              ["|", " ", " ", " ", " ", " ", " ", " ", "|"],
+              ["|", " ", " ", " ", " ", " ", " ", " ", "|"],
+              ["-", "-", "-", "-", "-", "-", "-", "-", "-"]]
+
+
+while True:
+    begin = input("Input command: ").split()
+
+    if begin[0] == "exit":
+        begin = ['', '', '']
+        break
+
+    elif len(begin) != 3:
+        print("Bad parameters!")
+        continue
+
+    break
+
 
 def print_field(field):  # printing gamefield
     for k in range(0, 9):
@@ -16,92 +36,216 @@ def print_field(field):  # printing gamefield
     print()
 
 
-game_field = [["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-              ["|", " ", " ", " ", " ", " ", " ", " ", "|"],
-              ["|", " ", " ", " ", " ", " ", " ", " ", "|"],
-              ["|", " ", " ", " ", " ", " ", " ", " ", "|"],
-              ["-", "-", "-", "-", "-", "-", "-", "-", "-"]]
-
-
-print_field(game_field)
-
-while True:
-    coor = input("Enter the coordinates: ")
-
-    try:
-        x_move = int(coor[0])
-        y_move = int(coor[2])
-    except ValueError:
-        print("You should enter numbers!")
-        continue
-
-    if x_move < 1 or y_move < 1:
-        print("Coordinates should be from 1 to 3!")
-        continue
-    elif x_move > 3 or y_move > 3:
-        print("Coordinates should be from 1 to 3!")
-        continue
-
-    if game_field[x_move][y_move * 2] == "X" or game_field[x_move][y_move * 2] == "O":
-        print("This cell is occupied! Choose another one!")
-        continue
-
-    game_field[x_move][y_move * 2] = "X"
-    print_field(game_field)
-
+def easy_move(field):  # computers move on easy level
     print("Making move level \"easy\"")
     x_coor = randrange(1, 4)
     y_coor = randrange(1, 4)
 
-    while x_coor == x_move and y_coor == y_move:
+    global moves_count
+
+    while field[x_coor][y_coor * 2] == "X" or field[x_coor][y_coor * 2] == "O":
         x_coor = randrange(1, 4)
         y_coor = randrange(1, 4)
 
-    game_field[x_coor][y_coor * 2] = "O"
-    print_field(game_field)
+    if moves_count % 2 == 1:
+        field[x_coor][y_coor * 2] = "O"
+        print_field(field)
+        moves_count += 1
+    else:
+        field[x_coor][y_coor * 2] = "X"
+        print_field(field)
+        moves_count += 1
 
-    # rules
-    for i in range(1, 4):
-        row = game_field[i]
-        if row[2] == " " or row[4] == " " or row[6] == " ":
-            victory = False
-            pass
+
+def first_user_move(field):
+    while True:
+        coor = input("Enter the coordinates: ")
+
+        try:
+            x_move = int(coor[0])
+            y_move = int(coor[2])
+        except ValueError:
+            print("You should enter numbers!")
+            continue
+
+        if x_move < 1 or y_move < 1:
+            print("Coordinates should be from 1 to 3!")
+            continue
+        elif x_move > 3 or y_move > 3:
+            print("Coordinates should be from 1 to 3!")
+            continue
+
+        if field[x_move][y_move * 2] == "X" or field[x_move][y_move * 2] == "O":
+            print("This cell is occupied! Choose another one!")
+            continue
+
+        field[x_move][y_move * 2] = "X"
+        break
+
+
+def user_move(field):
+    global moves_count
+    while True:
+        coor = input("Enter the coordinates: ")
+
+        try:
+            x_move = int(coor[0])
+            y_move = int(coor[2])
+        except ValueError:
+            print("You should enter numbers!")
+            continue
+
+        if x_move < 1 or y_move < 1:
+            print("Coordinates should be from 1 to 3!")
+            continue
+        elif x_move > 3 or y_move > 3:
+            print("Coordinates should be from 1 to 3!")
+            continue
+
+        if field[x_move][y_move * 2] == "X" or field[x_move][y_move * 2] == "O":
+            print("This cell is occupied! Choose another one!")
+            continue
+
+        if moves_count % 2 == 1:
+            field[x_move][y_move * 2] = "O"
+            print_field(field)
+            moves_count += 1
+            break
         else:
-            if row[2] == row[4] == row[6] == "X":
-                victory = True
-                print("X wins")
-                break
-            elif row[2] == row[4] == row[6] == "O":
-                victory = True
-                print("O wins")
-                break
+            field[x_move][y_move * 2] = "X"
+            print_field(field)
+            moves_count += 1
+            break
 
-    if victory is True:
+
+def easy_first_move(field):
+    print_field(field)
+
+    print("Making move level \"easy\"")
+    first_x_move = randrange(1, 4)
+    first_y_move = randrange(1, 4)
+    field[first_x_move][first_y_move * 2] = "X"
+    print_field(field)
+
+
+def victory_rules(field):
+    global victory
+    while True:
+
+        for i in range(1, 4):
+            row = field[i]
+            if row[2] == " " or row[4] == " " or row[6] == " ":
+                victory = False
+                pass
+            else:
+                if row[2] == row[4] == row[6] == "X":
+                    victory = True
+                    print("X wins")
+                    break
+                elif row[2] == row[4] == row[6] == "O":
+                    victory = True
+                    print("O wins")
+                    break
+
+        if victory is True:
+            break
+
+        for i in range(2, 7, 2):
+            if field[1][i] == " " or field[2][i] == " " or field[3][i] == " ":
+                victory = False
+                pass
+            else:
+                if field[2] == field[4] == field[6] == "X":
+                    victory = True
+                    print("X wins")
+                    break
+                elif field[2] == field[4] == field[6] == "O":
+                    victory = True
+                    print("O wins")
+                    break
+
+        if victory is True:
+            break
+
+        if field[1][2] == field[2][4] == field[3][6] == "X" or \
+                field[1][6] == field[2][4] == field[3][2] == "X":
+            print("X wins")
+            break
+
+        elif field[1][2] == field[2][4] == field[3][6] == "O" or \
+                field[1][6] == field[2][4] == field[3][2] == "O":
+            print("O wins")
+            break
+
         break
 
-    for i in range(2, 7, 2):
-        if game_field[1][i] == " " or game_field[2][i] == " " or game_field[3][i] == " ":
-            victory = False
-            pass
-        else:
-            if game_field[2] == game_field[4] == game_field[6] == "X":
-                victory = True
-                print("X wins")
-                break
-            elif game_field[2] == game_field[4] == game_field[6] == "O":
-                victory = True
-                print("O wins")
-                break
 
-    if victory is True:
-        break
+if begin[1] == "easy":
 
-    if game_field[1][2] == game_field[2][4] == game_field[3][6] == "X" or \
-            game_field[1][6] == game_field[2][4] == game_field[3][2] == "X":
-        print("X wins")
-        break
+    if begin[2] == "easy":
 
-    elif game_field[1][2] == game_field[2][4] == game_field[3][6] == "O" or \
-            game_field[1][6] == game_field[2][4] == game_field[3][2] == "O":
-        print("O wins")
-        break
+        easy_first_move(game_field)
+        moves_count = 1
+        victory = False
+
+        while moves_count < 9:
+            easy_move(game_field)
+            if moves_count >= 5:
+                victory_rules(game_field)
+                if victory is True:
+                    break
+
+    elif begin[2] == "user":
+
+        easy_first_move(game_field)
+        moves_count = 1
+        victory = False
+
+        while moves_count < 9:
+            user_move(game_field)
+            if moves_count >= 5:
+                victory_rules(game_field)
+                if victory is True:
+                    break
+            easy_move(game_field)
+            if moves_count >= 5:
+                victory_rules(game_field)
+                if victory is True:
+                    break
+
+elif begin[1] == "user":
+
+    if begin[2] == "easy":
+        print_field(game_field)
+
+        first_user_move(game_field)
+        print_field(game_field)
+        moves_count = 1
+        victory = False
+
+        while moves_count < 9:
+            easy_move(game_field)
+            if moves_count >= 5:
+                victory_rules(game_field)
+                if victory is True:
+                    break
+            user_move(game_field)
+            if moves_count >= 5:
+                victory_rules(game_field)
+                if victory is True:
+                    break
+
+    elif begin[2] == "user":
+        print_field(game_field)
+
+        first_user_move(game_field)
+        print_field(game_field)
+        moves_count = 1
+        victory = False
+
+        while moves_count < 9:
+            user_move(game_field)
+            if moves_count >= 5:
+                victory_rules(game_field)
+                if victory is True:
+                    break
